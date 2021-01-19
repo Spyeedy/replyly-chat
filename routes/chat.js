7769c5516b853667147f7ce931234bd098ac208b
Spyeedy <spyeedy8@gmail.com>
2021-01-19 14:39:16 +0800

    Add event for form submission to send a new message

--- routes/chat.js
+++ routes/chat.js
@@ -3,23 +3,27 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render('chat');
+    res.render("chat");
 });
 
 module.exports.router = router;
 module.exports.chatSocket = (io) => {
-    io.on('connection', (socket) => {
+    io.on("connection", (socket) => {
         console.log("Successful socket connection");
     
         // Welcome current user
-        socket.emit('message', 'Welcome to Replyly');
+        socket.emit("message", "Welcome to Replyly");
     
         // Broadcast when a user connects
-        socket.broadcast.emit('joinMessage', 'A user has joined the chat');
+        socket.broadcast.emit("joinMessage", "A user has joined the chat");
     
         // Runs when client disconnects
-        socket.on('disconnect', () => {
-            io.emit('userLeft', 'A user has left the chat');
-        });
+        socket.on("disconnect", () => {
+            io.emit("userLeft", "A user has left the chat");
+		});
+		
+		socket.on("newMessage", (message) => {
+			console.log("newMessage:",message);
+		})
     })
 };
