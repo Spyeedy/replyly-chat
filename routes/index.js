3af47747b83e56015933bead719f23226b49ad38
Spyeedy <spyeedy8@gmail.com>
2021-01-20 08:43:44 +0800

    Retrieve all rooms in index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { Room } = require("../database/models");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render('index');
+router.get("/", async(req, res) => {
+	const rooms = await Room.findAll();
+    res.render('index', { rooms });
 });
 
 module.exports = router;
