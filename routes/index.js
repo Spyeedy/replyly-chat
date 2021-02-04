4c34d60212ea61051e48a80486b806bc1047aec8
Spyeedy <spyeedy8@gmail.com>
2021-02-05 00:55:15 +0800

    Add route for login

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ const router = express.Router();
 
 router.get("/", async(req, res) => {
 	const rooms = await Room.findAll();
-    res.render('index', { rooms });
+    res.render("index", { rooms });
 });
 
 router.get("/join-chat", (req, res) => {
-	res.render('join-chat');
+	res.render("join-chat");
+});
+
+router.get("/login", (req, res) => {
+	res.render("login");
 })
 
 module.exports = router;
